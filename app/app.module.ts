635d1cb42fc5e1ae9076320fee0961c3a680384d
matskan <ivanmatskan@rambler.ru>
2021-03-14 13:11:44 +0300

    config

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { InjectionToken, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { TestComponent } from './test.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { TestComponent } from "./test.component";
+
+export interface IConfig {
+  medium: number;
+  large: number;
+}
+
+const config: IConfig = {
+  large: 1240,
+  medium: 1024
+};
+
+const ConfigToken = new InjectionToken<IConfig>("config");
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, TestComponent],
+  providers: [{ provide: ConfigToken, useValue: config }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- app/utils/utils.module.ts
+++ app/utils/utils.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ViewportService } from './viewport.service';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: []
+  declarations: [],
+  providers: [ViewportService]
 })
 export class UtilsModule {}
